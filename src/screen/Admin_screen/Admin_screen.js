e25c9d5ab81e166f8167569dc247670e2fd80077
ankit436 <89198098+ankit436@users.noreply.github.com>
2023-03-28 05:43:16 +0530

    Update Admin_screen.js

--- src/screen/Admin_screen/Admin_screen.js
+++ src/screen/Admin_screen/Admin_screen.js
@@ -3,6 +3,7 @@ import "./Admin_screen.css";
 import { useState, useEffect } from "react";
 import { getAllPendingApprovalByid, getAllapprovedclient } from "../../Service/Cruds";
 import {isUserLoggedIn } from  "../../Service/WindowAuthentication"
+import {admin_request} from "../../Model/Model"
 
 export default function Admin_screen() {
 
@@ -14,8 +15,9 @@ export default function Admin_screen() {
 
         const fetchData = async () => {
             try {
-                const data = await getAllPendingApprovalByid(isUserLoggedIn());
-               setAdminData(data);
+                const data = await getAllPendingApprovalByid(localStorage.getItem('user').substring(5));
+               setAdminData(data==null?[]:data);
+
             
                 setLoading(false);
             } catch (error) {
@@ -30,6 +32,15 @@ export default function Admin_screen() {
         return () => clearTimeout(timer);
     }, [adminData]);
 
+    const Approved =(item)=>{
+
+      console.log(item[admin_request[5].name])
+      item[admin_request[5].name]="aceess"
+      const newstate=adminData;
+      const index= adminData.findIndex((i)=>i[admin_request[8].name]===item[admin_request[8].name])
+      newstate[index]=item;
+      setAdminData(newstate)
+    }
 
   return (
     <div className="Admin_container">
@@ -53,54 +64,29 @@ export default function Admin_screen() {
             </div>
             <div className="Admin_permission_table_header_item">Action</div>
           </div>
-          <div className="Admin_permission_table_body">
-            <div className="Admin_permission_table_body_item">1</div>
-            <div className="Admin_permission_table_body_item">
-              <div className="user_icon_with_Name">
-                <div className="user_icon"></div>
-                <span className="user_name">Ankit</span>
-              </div>
-            </div>
-            <div className="Admin_permission_table_body_item">
-              ankitkumar700413.com
-            </div>
-            <div className="Admin_permission_table_body_item">High</div>
-            <div className="Admin_permission_table_body_item">open</div>
-            <div className="Admin_permission_table_body_item">276532532</div>
-            <div className="Admin_permission_table_body_item">
-              Client Approval
-            </div>
-            <div className="Admin_permission_table_body_item">
-            <div className='Permission_button'>
-                            <button className='button Approve_button'>Access</button>
-                            <button className='button Reject_button'>Reject</button>
-                        </div>
-            </div>
-          </div>
-
-            {adminData.map((item ,index) => {
+            {adminData.length>0&&adminData.map((item ,index) => {
                 return (
                     <div key={index} className="Admin_permission_table_body">
-                        <div className="Admin_permission_table_body_item">{item.id}</div>
+                        <div className="Admin_permission_table_body_item">{item[admin_request[0].name]}</div>
                         <div className="Admin_permission_table_body_item">
                             <div className="user_icon_with_Name">
                                 <div className="user_icon"></div>
-                                <span className="user_name">{item.name}</span>
+                                <span className="user_name">{item[admin_request[1].name]}</span>
                             </div>
                         </div>
                         <div className="Admin_permission_table_body_item">
-                            {item.Email}
+                        {item[admin_request[2].name]}
                         </div>
-                        <div className="Admin_permission_table_body_item">{item.Priority}</div>
-                        <div className="Admin_permission_table_body_item">{item.Status}</div>
-                        <div className="Admin_permission_table_body_item">{item.Created_date}</div>
+                        <div className="Admin_permission_table_body_item">{item[admin_request[3].name]}</div>
+                        <div className="Admin_permission_table_body_item">{item[admin_request[4].name]}</div>
+                        <div className="Admin_permission_table_body_item">{item[admin_request[5].name]}</div>
                         <div className="Admin_permission_table_body_item">
-                            {item.Description}
+                        {item[admin_request[6].name]}
                         </div>
                         <div className="Admin_permission_table_body_item">
                         <div className="Admin_permission_table_body_item">
                         <div className='Permission_button'>
-                            <button className='button Approve_button'>Access</button>
+                            <button className='button Approve_button' onClick={()=>Approved(item)}  >Access</button>
                             <button className='button Reject_button'>Reject</button>
                         </div>
                     </div>
