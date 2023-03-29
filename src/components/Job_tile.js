766aaea987e4618ea7a4a4fa279539e1d6eb6910
ankit435 <ankitkumarsingh435@gmail.com>
2023-03-29 17:34:26 +0530

    8.13

--- src/components/Job_tile.js
+++ src/components/Job_tile.js
@@ -5,12 +5,13 @@ import "./job_tile.css";
 import { Project_detail } from "../Model/Model";
 
 const Job_tile = ({ job }) => {
+  console.log(job);
   return (
     <div className="job_list_container">
-      {Project_detail.map((job) => {
+      { job.length>0&& Project_detail.map((job,index) => {
         return (
           <Link to="/Job-detail/2" className="link">
-            <div className="job_tiles">
+            <div className="job_tiles" key={index}>
               <div className="job_dropdown">
                 <i className="dropdown_icon"></i>
                 <div className="Add_to_bookmark_icon book_mark_icon_filled"></div>
@@ -21,11 +22,11 @@ const Job_tile = ({ job }) => {
               <div className="job_data">
                 <div className="job_title">
                   {" "}
-                  <h3>tuiewu hsgds wiiw  wije riur</h3>
+                  <h3>{job[Project_detail[4].name]}</h3>
                 </div>
                 <div className="job_description scroll">
                   {" "}
-                  <p>wgetuiwiwgiuewsdhiodhasdbjkasbdjwm wel e jbewr 3 uhh4h3h3 rgrgeg regregregrervevrurerpoerwehok  jd ew we ew j we jwje    wejhiohe e e oe w  rfewjwoi geurg iwgiuewsdhiodhasdbjkasbdjwm wel e jbewr 3 uhh4h3h3 rgrgeg regregregrervevrurerpoerwehok  jd ew we ew j we jwje    wejhiohe e e oe w  rfewjwoi geurgeurogoeurogoe</p>
+                  <p>{job[Project_detail[6].name]}</p>
                 </div>
               </div>
 
@@ -36,7 +37,7 @@ const Job_tile = ({ job }) => {
               <div className="Job_group_date_icon">
                 <div className="Job_Date">
                   <i className="view_Date"></i>{" "}
-                  <span className="view_Date_span">Date: 01:02:2034</span>
+                  <span className="view_Date_span">Date: {job[Project_detail[7].name]}</span>
                 </div>
                 <div className="Job_group_icon"></div>
               </div>
