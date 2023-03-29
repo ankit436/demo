4f7359ca1dbea9ba6de1a9db3fcaf60b213af414
ankit436 <89198098+ankit436@users.noreply.github.com>
2023-03-29 19:27:02 +0530

    Update Active.js

--- src/screen/JOB_SCREEN/Active.js
+++ src/screen/JOB_SCREEN/Active.js
@@ -1,17 +1,45 @@
 
 
-
-
-
 import "./job_screen.css";
-import Job_tile from "../../components/Job_tile";
+import React from "react";
+import "../../Helpers/Loader.css"
 
+
+import Job_tile from "../../components/Job_tile";
+import axios from "axios";
+
+import {getallactivejob} from "../../Service/Cruds"
 
 export default function Active_job() {
 
+  const [job_list, setJob_list] = React.useState([]);
+  const [loadingState, setLoadingState] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => { 
+
+    const fetchData = async () => {
+      try {
+        const data = await getallactivejob({project_state:0,project_status:0});
+        setJob_list(data == null ? [] : data);
+       
+        setLoadingState(false);
+      } catch (error) {
+        setLoadingState(false);
+        setError(error);
+      }
+    };
+    const timer = setTimeout(() => {
+      fetchData();
+    }, 3000);
+
+    return ()=>clearTimeout(timer)
+
+   }, [job_list]);
+
     return (
         <div className="job_list">
-          <Job_tile />
+           {loadingState?<div className="loading">  <span class="loading__anim"></span></div>: <Job_tile job ={job_list} />}
         </div> 
     )
 
