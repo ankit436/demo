e4fdb99762f3d071625f7d66f5ed20b72c06ec3f
ankit436 <89198098+ankit436@users.noreply.github.com>
2023-05-22 17:28:48 +0530

    Update Temp.js

--- src/Temp.js
+++ src/Temp.js
@@ -40,4 +40,42 @@ const ExcelDownloader = ({ data, fileName }) => {
 
 };
 
-export default ExcelDownloader;
+export default ExcelDownloader;const downloadExcel = () => {
+  // Fetch API data
+  fetch('https://api.example.com/data')
+    .then(response => response.json())
+    .then(data => {
+      // Select desired columns from API data
+      const selectedColumns = data.map(item => {
+        return {
+          id: item.id,
+          name: item.name,
+          // Add more properties as needed
+        };
+      });
+
+      // Create a new workbook and worksheet
+      const workbook = XLSX.utils.book_new();
+      const worksheet = XLSX.utils.json_to_sheet(selectedColumns);
+
+      // Add the worksheet to the workbook
+      XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet 1');
+
+      // Convert the workbook to an Excel file buffer
+      const excelBuffer = XLSX.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+
+      // Create a Blob from the buffer
+      const blob = new Blob([excelBuffer], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
+
+      // Save the file using FileSaver.js
+      saveAs(blob, 'api_data.xlsx');
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+};
