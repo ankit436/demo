06a1ae0e73a917ca3c923ba5612a1441a28c9e84
ankit436 <89198098+ankit436@users.noreply.github.com>
2023-06-10 12:47:09 +0530

    Update States.js

--- src/States.js
+++ src/States.js
@@ -1,6 +1,68 @@
 bh
 
-g
+h
+bh
+
+
+
+
+import { useLocation, useHistory, useEffect } from 'react-router-dom';
+
+function MyComponent() {
+
+  const location = useLocation();
+
+  const history = useHistory();
+
+  useEffect(() => {
+
+    // Function to destroy the props state
+
+    const destroyPropsState = () => {
+
+      history.replace({
+
+        ...location,
+
+        state: {} // Replace the state with an empty object
+
+      });
+
+    };
+
+    // Listen for changes in the pathname
+
+    const unlisten = history.listen((newLocation) => {
+
+      if (location.pathname !== newLocation.pathname) {
+
+        destroyPropsState();
+
+      }
+
+    });
+
+    // Clean up the listener when the component unmounts
+
+    return () => {
+
+      unlisten();
+
+    };
+
+  }, [location, history]);
+
+  return (
+
+    <div>
+
+      {/* Rest of your component */}
+
+    </div>
+
+  );
+
+}
 
 
 
