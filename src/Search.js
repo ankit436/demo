dc3ae8e54061c9a661d325e9eca7b167132c0c4b
Ankit Kumar <70105136+ankit435@users.noreply.github.com>
2023-06-16 17:39:22 +0530

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,208 +1,75 @@
-import React, { useState, useEffect } from 'react';
+function AutocompleteAsync() {
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [inputValue, setInputValue] = useState('');
 
-const AsyncSearch = ({ apiUrl, placeholder }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const fetchSuggestions = async (query) => {
+    setLoading(true);
 
-  useEffect(() => {
-    let isMounted = true;
-
-    const search = async () => {
-      setIsLoading(true);
-      setError(null);
-
-      try {
-        const response = await fetch(`${apiUrl}?term=${searchTerm}`);
-        const data = await response.json();
-
-        if (isMounted) {
-          setSearchResults(data.results);
-        }
-      } catch (error) {
-        if (isMounted) {
-          setError('Error fetching search results.');
-        }
-      } finally {
-        if (isMounted) {
-          setIsLoading(false);
-        }
-      }
-    };
-
-    if (searchTerm !== '') {
-      search();
-    } else {
-      setSearchResults([]);
+    try {
+      const response = await axios.get(`API_URL?q=${query}`);
+      const data = response.data;
+      setOptions(data.suggestions);
+    } catch (error) {
+      console.error('Error fetching suggestions:', error);
+      setOptions([]);
     }
 
-    return () => {
-      isMounted = false;
-    };
-  }, [apiUrl, searchTerm]);
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+    setLoading(false);
   };
+
+  const handleInputChange = (event, value) => {
+    setInputValue(value);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSelect = (event, value) => {
+    console.log('Selected value:', value);
+  };
+
+  const handleRenderOption = (option) => {
+    return <div>{option}</div>;
+  };
+
+  const handleRenderInput = (params) => (
+    <TextField
+      {...params}
+      label="Search"
+      InputProps={{
+        ...params.InputProps,
+        endAdornment: (
+          <>
+            {loading && <CircularProgress color="inherit" size={20} />}
+            {params.InputProps.endAdornment}
+          </>
+        ),
+      }}
+    />
+  );
+
+  const handleInputChangeDebounced = debounce(fetchSuggestions, 300);
 
   return (
-    <div>
-      <input type="text" placeholder={placeholder} value={searchTerm} onChange={handleSearch} />
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : searchResults.length > 0 ? (
-        <ul>
-          {searchResults.map((result) => (
-            <li key={result.id}>{result.title}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No results found</p>
-      )}
-    </div>
+    <Autocomplete
+      open={open}
+      onOpen={handleOpen}
+      onClose={handleClose}
+      inputValue={inputValue}
+      onInputChange={handleInputChange}
+      options={options}
+      loading={loading}
+      renderOption={handleRenderOption}
+      renderInput={handleRenderInput}
+      onKeyUp={() => handleInputChangeDebounced(inputValue)}
+      onSelect={handleSelect}
+    />
   );
-};
-
-export default AsyncSearch;
-
-
-
-
-/* AsyncSearch.css */
-
-.async-search-results {
-  list-style: none;
-  padding: 0;
-}
-
-.async-search-results li {
-  margin-bottom: 5px;
-  background-color: #f2f2f2;
-  padding: 10px;
-  border-radius: 5px;
-}
-
-.async-search-results li:hover {
-  background-color: #e0e0e0;
-}
-
-
-
-
-
-import React, { useState, useEffect } from 'react';
-
-const AsyncSearch = ({ apiUrl, placeholder }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchCategory, setSearchCategory] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    let isMounted = true;
-
-    const search = async () => {
-      setIsLoading(true);
-      setError(null);
-
-      try {
-        const response = await fetch(`${apiUrl}?term=${searchTerm}&category=${searchCategory}`);
-        const data = await response.json();
-
-        if (isMounted) {
-          setSearchResults(data.results);
-        }
-      } catch (error) {
-        if (isMounted) {
-          setError('Error fetching search results.');
-        }
-      } finally {
-        if (isMounted) {
-          setIsLoading(false);
-        }
-      }
-    };
-
-    if (searchTerm !== '' && searchCategory !== '') {
-      search();
-    } else {
-      setSearchResults([]);
-    }
-
-    return () => {
-      isMounted = false;
-    };
-  }, [apiUrl, searchTerm, searchCategory]);
-
-  const handleSearchTermChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleCategoryChange = (event) => {
-    setSearchCategory(event.target.value);
-  };
-
-  return (
-    <div>
-      <input type="text" placeholder={placeholder} value={searchTerm} onChange={handleSearchTermChange} />
-      <select value={searchCategory} onChange={handleCategoryChange}>
-        <option value="">Select Category</option>
-        <option value="category1">Category 1</option>
-        <option value="category2">Category 2</option>
-        <option value="category3">Category 3</option>
-      </select>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : searchResults.length > 0 ? (
-        <ul>
-          {searchResults.map((result) => (
-            <li key={result.id}>{result.title}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No results found</p>
-      )}
-    </div>
-  );
-};
-
-
-
-
-
-hshsh
-export default AsyncSearch;
-hhssb
-bsbsn
-
-
-
-return (
-    <div>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={handleInputChange}
-        placeholder="Search..."
-      />
-      <div>
-        <select>
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      </div>
-    </div>
-  );
-}
-
-
-  
+            }
