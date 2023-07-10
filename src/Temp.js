a58dc2fbb3db9d52ae905c171af4d58bea3a23b7
Ankit Kumar <70105136+ankit435@users.noreply.github.com>
2023-07-10 13:05:35 +0530

    Update Temp.js

--- src/Temp.js
+++ src/Temp.js
@@ -1,4 +1,13 @@
 
+public class CorsSettings
+{
+    public string PolicyName { get; set; }
+    public string[] AllowedOrigins { get; set; }
+    public string[] AllowedMethods { get; set; }
+    public string[] AllowedHeaders { get; set; }
+}
+
+
 {
   "Cors": {
     "PolicyName": "MyCorsPolicy",
