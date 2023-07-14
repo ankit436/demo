73370280db575d14c35e3d9fb721278f5e210f64
Ankit Kumar <70105136+ankit435@users.noreply.github.com>
2023-07-14 08:56:48 +0530

    Update Temp.js

--- src/Temp.js
+++ src/Temp.js
@@ -1,3 +1,37 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useDataFetching = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useDataFetching;
+
+
+
+
+
 
 import { makeStyles } from '@mui/styles';
 
